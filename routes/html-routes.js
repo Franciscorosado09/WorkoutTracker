51aba239c267eff9edbe66d0d8b7adc2dbbb24ec
Francisco Rosado <Franciscorosado09@gmail.com>
2021-03-17 14:17:21 -0400

    added to server, setting up routes then models

---
+++ routes/html-routes.js
@@ -0,0 +1,26 @@
+const path = require("path")
+
+module.exports = (app) => {
+
+ app.get('/', (req, res) =>
+ res.sendFile(path.join(__dirname, '../public/index.html'))
+ 
+ );
+
+ app.get('/excercise', (req, res) =>
+ res.sendFile(path.join(__dirname, '../public/excercise.html'))
+ 
+ );
+
+
+ app.get('/stats', (req, res) =>
+ res.sendFile(path.join(__dirname, '../public/stats.html'))
+ 
+ );
+
+
+
+
+
+
+}
--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+const API = require ("./public/api")
+
+
+// const db = require("./models");
 
 const app = express();
 
@@ -14,19 +17,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(express.static("seeders"))
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+   
     useFindAndModify: false,
   }
 );
 //need to attach routes
 
-
+require("./routes/api-routes.js")(app)
+require("./routes/html-routes.js")(app)
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
