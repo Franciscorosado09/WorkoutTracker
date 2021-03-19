c7ce86619e44fad8c2d8df6c6268b83351e53a5b
Francisco Rosado <Franciscorosado09@gmail.com>
2021-03-19 18:33:57 -0400

    fixed issues completely working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,157 +1,106 @@
-const Workouts = require('../models/workouts')
+const Workouts = require("../models/workouts");
 // console.log (Workouts)
 
 module.exports = (app) => {
+  // app.get("/api/workouts", (req, res) => {
+  //     db.Workouts.find({})
+  //       .then(dbWorkouts => {
+  //         res.json(dbWorkouts);
+  //       })
+  //       .catch(err => {
+  //         res.json(err);
+  //       });
+  //   });
 
+  // need get
 
-
-    // app.get("/api/workouts", (req, res) => {
-    //     db.Workouts.find({})
-    //       .then(dbWorkouts => {
-    //         res.json(dbWorkouts);
-    //       })
-    //       .catch(err => {
-    //         res.json(err);
-    //       });
-    //   });
-
-
-
-    // need get
-
-    app.get("/api/workouts", (req, res) => {
-        console.log("test GET")
-        Workouts.aggregate([{
-                    $limit: 7
-                },
-                {
-                    $addFields: {
-                        totalDuration: {
-                            $sum: "$exercise.duration"
-                        }
-
-                    }
-
-                },
-
-            ])
-            .then(dbWorkouts => {
-                console.log(dbWorkouts);
-                res.json(dbWorkouts);
-            })
-            .catch(err => {
-                res.json(err)
-            })
-
-
-
-
-
-    })
-
-    // //range
-    app.get("/api/workouts/range", (req, res) => {
-        console.log(res)
-
-        Workouts.aggregate([{ $limit: 7 },
-        {
-            $addFields: {
-                totalDuration: {
-                    $sum: "$exercises.duration"
-
-                }
-            }
-
+  app.get("/api/workouts", (req, res) => {
+    console.log("test GET");
+    Workouts.aggregate([
+      {
+        $limit: 7,
+      },
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercise.duration",
+          },
         },
+      },
+    ])
+      .then((dbWorkouts) => {
+        console.log(dbWorkouts);
+        res.json(dbWorkouts);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-        ])
-            .then(dbWorkouts => {
-                res.json(dbWorkouts)
-            })
-            .catch(err => {
-                res.status(err)
-            })
+  // //range
+  app.get("/api/workouts/range", (req, res) => {
+    console.log(res);
+
+    Workouts.aggregate([
+      { $limit: 7 },
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ])
+      .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+      })
+      .catch((err) => {
+        res.status(err);
+      });
+  });
+
+  // post a workout
+  app.post("/api/workouts", (req, res) => {
+    console.log("Hello World");
+    Workouts.create({
+      exercises: [req.body],
     })
+      .then((dbWorkouts) => {
+        console.log("good");
+        res.json(dbWorkouts);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
+  // put
 
+  app.put("/api/workouts/:id", (req, res) => {
+    console.log(req);
+    console.log("test put");
+    Workouts.findOneAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        $push: {
+          exercises: req.body,
+        },
+      },
 
-
-
-
-
-    // post a workout
-    app.post("/api/workouts", (body, res) => {
-        console.log(body)
-        console.log('Hello World')
-        Workouts.create(body)
-            .then(dbWorkouts => {
-                console.log(body)
-                res.json(dbWorkouts);
-            })
-            .catch(err => {
-                res.json(err);
-            });
-    });
-
-
-    // put 
-
-
-
-
-    app.put("/api/workouts/:id", (req, res) => {
-            console.log(req)
-            console.log('test put')
-            Workouts.findOneAndUpdate({
-             _id: req.params.id
-            }, 
-            {
-                $push: 
-                {
-                    exercises: req.body
-                }
-            }, 
-            
-            {
-                new: true
-            }
-
-        ).then(dbWorkouts => {
-            // console.log("test")
-            // console.log(req.body)
-            // console.log(req.params.id)
-            res.json(dbWorkouts);
-        }).catch(err => {
-            res.json(err)
-        })
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+      {
+        new: true,
+      }
+    )
+      .then((dbWorkouts) => {
+        // console.log("test")
+        // console.log(req.body)
+        // console.log(req.params.id)
+        res.json(dbWorkouts);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
